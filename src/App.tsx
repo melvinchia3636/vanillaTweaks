eda3a35567ddf6ce0929ea18c3e049bd893af1f8
melvinchia3636 <melvinchia623600@gmail.com>
2022-03-09 16:30:12 +0800

    lots of progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import ResourcePack from './components/ResourcePacks';
+import Datapacks from './components/Datapacks';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="App w-full h-full flex flex-col overflow-hidden">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resource-packs" element={<ResourcePack />} />
+          <Route path="/datapacks" element={<Datapacks />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
